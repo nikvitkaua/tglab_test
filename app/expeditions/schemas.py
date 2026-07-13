28aa7c15eff4968b40763e92e5b774d376bf3aa8
Mykyta <nikvitka@MacBook-Air-Mykyta.local>
2026-07-13 22:07:59 +0300

    add: expeditions schemas

---
+++ app/expeditions/schemas.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+from app.expeditions.models import ExpeditionStatus, MemberState
+from app.users.schemas import UserResponse
+
+
+class ExpeditionBase(BaseModel):
+    title: str = Field(..., min_length=3, max_length=100)
+    description: str | None = None
+    start_at: datetime
+    end_at: datetime | None = None
+    capacity: int = Field(..., gt=1, description="Місткість експедиції має бути більшою за 1")
+
+
+class ExpeditionCreate(ExpeditionBase):
+    pass
+
+
+class ExpeditionMemberResponse(BaseModel):
+    id: int
+    user_id: int
+    state: MemberState
+    invited_at: datetime
+    confirmed_at: datetime | None = None
+    user: UserResponse
+
+    class Config:
+        from_attributes = True
+
+
+class ExpeditionResponse(ExpeditionBase):
+    id: int
+    status: ExpeditionStatus
+    chief_id: int
+    created_at: datetime
+    updated_at: datetime
+    members: list[ExpeditionMemberResponse] = []
+
+    class Config:
+        from_attributes = True
+
+
+class InvitationCreate(BaseModel):
+    user_id: int
